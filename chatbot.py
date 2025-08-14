d4b118bdd956862976cbe03eeb01f0cf31282e82
frogface539 <lakshayjain632@gmail.com>
2025-08-14 12:31:09 +0530

    first commit

--- chatbot.py
+++ chatbot.py
@@ -7,9 +7,9 @@ import streamlit as st
 
 load_dotenv()
 
-os.environ["LANCHAIN_API_KEY"] = os.getenv("LANGCHAIN_API_KEY")
+os.environ["LANGCHAIN_API_KEY"] = os.getenv("LANGCHAIN_API_KEY")
 os.environ['LANGCHAIN_TRACING_V2'] = 'true'
-os.environ['LANGCHAIN_PROJECT'] = os.getenv('LANGCHAIN_PROJECT')\
+os.environ['LANGCHAIN_PROJECT'] = os.getenv('LANGCHAIN_PROJECT')
 
 prompt = ChatPromptTemplate.from_messages(  
     [
